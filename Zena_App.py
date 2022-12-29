9de6840fa9d9cd80383f1659cfb5d1025180790f
Pavlin84 <58217926+Pavlin84@users.noreply.github.com>
2022-12-29 10:23:35 +0200

    test sf access

--- Zena_App.py
+++ Zena_App.py
@@ -4,4 +4,10 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('Snowflake Badge')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
